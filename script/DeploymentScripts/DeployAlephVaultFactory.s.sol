034f5c67fb939103a82f29062a3ffb7b61db3472
0xpanicError <theyashsaraswat@gmail.com>
2025-08-13 13:59:33 +0530

    refactor: script

--- script/DeploymentScripts/DeployAlephVaultFactory.s.sol
+++ script/DeploymentScripts/DeployAlephVaultFactory.s.sol
@@ -39,15 +39,51 @@ contract DeployAlephVaultFactory is BaseScript {
         vm.createSelectFork(_chainId);
         string memory _environment = _getEnvironment();
 
-        address _beacon = _getBeacon(_chainId, _environment);
         address _proxyOwner = _getProxyOwner(_chainId, _environment);
 
         IAlephVaultFactory.InitializationParams memory _initializationParams;
 
         string memory _factoryConfig = _getFactoryConfig();
         string memory _deploymentConfig = _getDeploymentConfig();
-        _initializationParams = IAlephVaultFactory.InitializationParams({
-            beacon: _beacon,
+        _initializationParams = _getInitializationParams(_factoryConfig, _deploymentConfig, _chainId, _environment);
+
+        console.log("operationsMultisig", _initializationParams.operationsMultisig);
+        console.log("oracle", _initializationParams.oracle);
+        console.log("guardian", _initializationParams.guardian);
+        console.log("authSigner", _initializationParams.authSigner);
+        console.log("feeRecipient", _initializationParams.feeRecipient);
+        console.log("managementFee", _initializationParams.managementFee);
+        console.log("performanceFee", _initializationParams.performanceFee);
+
+        bytes memory _initializeArgs =
+            abi.encodeWithSelector(AlephVaultFactory.initialize.selector, _initializationParams);
+
+        uint256 _privateKey = _getPrivateKey();
+        vm.startBroadcast(_privateKey);
+        AlephVaultFactory _factoryImpl = new AlephVaultFactory();
+
+        ITransparentUpgradeableProxy _proxy = ITransparentUpgradeableProxy(
+            address(new TransparentUpgradeableProxy(address(_factoryImpl), _proxyOwner, _initializeArgs))
+        );
+
+        console.log("Factory deployed at:", address(_proxy));
+
+        _writeDeploymentConfig(
+            _chainId, _environment, ".factoryImplementationAddress", vm.toString(address(_factoryImpl))
+        );
+        _writeDeploymentConfig(_chainId, _environment, ".factoryProxyAddress", vm.toString(address(_proxy)));
+
+        vm.stopBroadcast();
+    }
+
+    function _getInitializationParams(
+        string memory _factoryConfig,
+        string memory _deploymentConfig,
+        string memory _chainId,
+        string memory _environment
+    ) internal view returns (IAlephVaultFactory.InitializationParams memory) {
+        return IAlephVaultFactory.InitializationParams({
+            beacon: _getBeacon(_chainId, _environment),
             operationsMultisig: vm.parseJsonAddress(
                 _factoryConfig, string.concat(".", _chainId, ".", _environment, ".operationsMultisig")
             ),
@@ -76,34 +112,5 @@ contract DeployAlephVaultFactory is BaseScript {
                 vm.parseJsonUint(_factoryConfig, string.concat(".", _chainId, ".", _environment, ".performanceFee"))
             )
         });
-
-        console.log("beacon", _beacon);
-        console.log("operationsMultisig", _initializationParams.operationsMultisig);
-        console.log("oracle", _initializationParams.oracle);
-        console.log("guardian", _initializationParams.guardian);
-        console.log("authSigner", _initializationParams.authSigner);
-        console.log("feeRecipient", _initializationParams.feeRecipient);
-        console.log("managementFee", _initializationParams.managementFee);
-        console.log("performanceFee", _initializationParams.performanceFee);
-
-        bytes memory _initializeArgs =
-            abi.encodeWithSelector(AlephVaultFactory.initialize.selector, _initializationParams);
-
-        uint256 _privateKey = _getPrivateKey();
-        vm.startBroadcast(_privateKey);
-        AlephVaultFactory _factoryImpl = new AlephVaultFactory();
-
-        ITransparentUpgradeableProxy _proxy = ITransparentUpgradeableProxy(
-            address(new TransparentUpgradeableProxy(address(_factoryImpl), _proxyOwner, _initializeArgs))
-        );
-
-        console.log("Factory deployed at:", address(_proxy));
-
-        _writeDeploymentConfig(
-            _chainId, _environment, ".factoryImplementationAddress", vm.toString(address(_factoryImpl))
-        );
-        _writeDeploymentConfig(_chainId, _environment, ".factoryProxyAddress", vm.toString(address(_proxy)));
-
-        vm.stopBroadcast();
     }
 }
